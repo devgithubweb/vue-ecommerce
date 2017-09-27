d3a7ecdbbf7c9fe649db096e2faae5a44ba29971
devgithubweb <doubleddhero@gmail.com>
2017-09-27 17:41:09 +0100

    use method call for getting token

--- src/services/UserInformationService.js
+++ src/services/UserInformationService.js
@@ -2,20 +2,21 @@
  * Created by Ibrahim on 19/09/2017.
  */
 import axios from 'axios'
+import Auth from './Auth'
 
 const API_URL = process.env.API_URL
 let token = localStorage.getItem('token')
 
 export default {
   getUserInformation () {
-    return axios.get(`${API_URL}user-information/`, {headers: {Authorization: token}})
+    return axios.get(`${API_URL}user-information/`, {headers: {Authorization: Auth.getToken()}})
       .then(response => {
         return response
       })
   },
 
   createUserInformation (userInfo) {
-    return axios.post(`${API_URL}user-information/`, userInfo, {headers: {Authorization: token}})
+    return axios.post(`${API_URL}user-information/`, userInfo, {headers: {Authorization: Auth.getToken()}})
       .then(response => {
         return response
       })
